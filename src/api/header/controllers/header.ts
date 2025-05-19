1b300e041c7d817c705f64c4a8a34a85f6b51a02
Amar Jain <amar@primuspartners.in>
2025-05-19 17:23:43 +0530

    updating header url and data

--- src/api/header/controllers/header.ts
+++ src/api/header/controllers/header.ts
@@ -1,7 +1,17 @@
-/**
- * header controller
- */
+import { factories } from '@strapi/strapi';
 
-import { factories } from '@strapi/strapi'
+export default factories.createCoreController('api::header.header', ({ strapi }) => ({
+  async find(ctx) {
+    const entries = await strapi.entityService.findMany('api::header.header', {
+      ...ctx.query,
+      populate: {
+        data: {
+          populate: '*', // this will get sub_menu inside each data item
+        },
+        logo: true, // if logo is a media field
+      },
+    });
 
-export default factories.createCoreController('api::header.header');
+    return { data: entries };
+  }
+}));
